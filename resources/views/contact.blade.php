8945bd7fe6e4764f2e59df7a1d5740e8ce3ea418
ms-hamid <hamid.albase@gmail.com>
2025-03-12 21:27:45 +0700

    Week2

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    Selamat Datang di Home
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+     Selamat Datang di Home {{$nama}}<br>
+     Anda adalah seorang {{$pekerjaan}}
+</div>
